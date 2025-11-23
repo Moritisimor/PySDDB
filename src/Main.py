669dc5d18e7513f3afacba12fbffb924e2e19d63
Moritisimor <devMoritisimor@proton.me>
2025-11-23 20:23:18 +0100

    Got rid of unnecessary JSONResponse constructor parameters

--- src/Main.py
+++ src/Main.py
@@ -8,24 +8,18 @@ app: fastapi.FastAPI = fastapi.FastAPI()
 
 @app.get("/")
 def pong() -> JSONResponse:
-    return JSONResponse(
-        content={ "message": "Hello Client!" },
-        status_code=200 # Ok
-    )
+    return JSONResponse({ "message": "Hello Client!" })
 
 @app.get("/get/{filename}")
 def read_json(filename: str) -> JSONResponse:
     try:
         with open(f"{filename}.json") as f:
-            return JSONResponse(
-                content=json.load(f),
-                status_code=200 # Ok
-            )
+            return JSONResponse(json.load(f))
 
     except OSError:
         return JSONResponse(
-            content={ "message": "The given file does not exist." },
-            status_code=404 # Not Found
+            { "message": "The given file does not exist." },
+            404 # Not Found
         )
 
 
@@ -36,27 +30,25 @@ def create_json(filename: str, data: dict) -> JSONResponse:
             json.dump(data, f)
 
         return JSONResponse(
-            content={ "message": "Successfully created file." },
-            status_code=201 # Created
+            { "message": "Successfully created file." },
+            201 # Created
         )
+    
     except OSError:
         return JSONResponse(
-            content={ "message": "Could not create file as it already exists." },
-            status_code=409 # Conflict
+            { "message": "Could not create file as it already exists." },
+            409 # Conflict
         )
 
 @app.delete("/drop/{filename}")
 def delete_json(filename: str) -> JSONResponse:
     try:
         os.remove(f"{filename}.json")
-        return JSONResponse(
-            content={ "message": "Successfully deleted file!" },
-            status_code=200 # Ok
-        )
+        return JSONResponse({ "message": "Successfully deleted file!" })
     except OSError:
         return JSONResponse(
-            content={ "message": "The given file does not exist." },
-            status_code=404 # Not Found
+            { "message": "The given file does not exist." },
+            404 # Not Found
         )
 
 if __name__ == "__main__":
