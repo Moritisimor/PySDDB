5a80ae1433883f63d3322cf61048bde47b3a8f22
Moritisimor <devMoritisimor@proton.me>
2025-12-03 21:32:09 +0100

    Added locks so data races and don't happen and made JSON writing atomic

--- src/Main.py
+++ src/Main.py
@@ -1,3 +1,4 @@
+import asyncio
 import uvicorn
 import fastapi
 import json
@@ -5,51 +6,56 @@ import os
 from fastapi.responses import JSONResponse
 
 app: fastapi.FastAPI = fastapi.FastAPI()
+lock: asyncio.Lock = asyncio.Lock()
 
 @app.get("/")
-def pong() -> JSONResponse:
+async def pong() -> JSONResponse:
     return JSONResponse({ "message": "Hello Client!" })
 
 @app.get("/get/{filename}")
-def read_json(filename: str) -> JSONResponse:
-    try:
-        with open(f"{filename}.json") as f:
-            return JSONResponse(json.load(f))
+async def read_json(filename: str) -> JSONResponse:
+    async with lock:
+        try:
+            with open(f"{filename}.json") as f:
+                return JSONResponse(json.load(f))
 
-    except OSError:
-        return JSONResponse(
-            { "message": "The given file does not exist." },
-            404 # Not Found
-        )
+        except OSError:
+            return JSONResponse(
+                { "message": "The given file does not exist." },
+                404 # Not Found
+            )
 
 
 @app.post("/create/{filename}")
-def create_json(filename: str, data: dict) -> JSONResponse:
-    try:
-        with open(f"{filename}.json", "x") as f:
-            json.dump(data, f)
+async def create_json(filename: str, data: dict) -> JSONResponse:
+    async with lock:
+        try:
+            with open(f"{filename}.json.tmp", "w") as f:
+                json.dump(data, f)
+                os.replace(f"{filename}.json.tmp", f"{filename}.json")
 
-        return JSONResponse(
-            { "message": "Successfully created file." },
-            201 # Created
-        )
-    
-    except OSError:
-        return JSONResponse(
-            { "message": "Could not create file as it already exists." },
-            409 # Conflict
-        )
+            return JSONResponse(
+                { "message": "Successfully created file." },
+                201 # Created
+            )
+        
+        except OSError:
+            return JSONResponse(
+                { "message": "Could not create file as it already exists." },
+                409 # Conflict
+            )
 
 @app.delete("/drop/{filename}")
-def delete_json(filename: str) -> JSONResponse:
-    try:
-        os.remove(f"{filename}.json")
-        return JSONResponse({ "message": "Successfully deleted file!" })
-    except OSError:
-        return JSONResponse(
-            { "message": "The given file does not exist." },
-            404 # Not Found
-        )
+async def delete_json(filename: str) -> JSONResponse:
+    async with lock:
+        try:
+            os.remove(f"{filename}.json")
+            return JSONResponse({ "message": "Successfully deleted file!" })
+        except OSError:
+            return JSONResponse(
+                { "message": "The given file does not exist." },
+                404 # Not Found
+            )
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
